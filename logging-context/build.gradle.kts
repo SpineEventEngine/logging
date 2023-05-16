567866e20943dccc92f79c5feabd5d26150b7980
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-05-16 14:23:32 +0100

    Optimise imports

--- logging-context/build.gradle.kts
+++ logging-context/build.gradle.kts
@@ -24,8 +24,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import io.spine.internal.dependency.JUnit
-
 plugins {
     `kotlin-jvm-module`
     `project-report`
