4f2dce651bacae41852ea6a09374be5f9d4996a4
Yevhenii Nadtochii <yevhenii.nadtochii@teamdev.com>
2023-07-17 15:42:48 +0300

    Init `api-tests` module with Kotlin-rewritten tests

--- flogger/api-tests/build.gradle.kts
+++ flogger/api-tests/build.gradle.kts
@@ -24,14 +24,39 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-rootProject.name = "flogger"
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
 
-include(
-    "api",
-    "api-tests",
-    "testing",
-    "platform-generator",
-    "system-backend",
-    "log4j2-backend",
-    "grpc-context",
-)
+dependencies {
+    testImplementation(project(":api"))
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+tasks {
+    withType<Test>().configureEach {
+        useJUnitPlatform()
+
+        fun TestResult.summary(): String =
+            """
+        Test summary:
+        >> $testCount tests
+        >> $successfulTestCount succeeded
+        >> $failedTestCount failed
+        >> $skippedTestCount skipped
+        """
+
+        afterSuite(
+            KotlinClosure2<TestDescriptor, TestResult, Unit>({ descriptor, result ->
+                if (descriptor.parent == null) {
+                    logger.lifecycle(result.summary())
+                }
+            })
+        )
+    }
+}
