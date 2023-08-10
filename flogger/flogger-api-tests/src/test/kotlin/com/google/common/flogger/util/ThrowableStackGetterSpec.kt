b943592f845a8729605a23dbed02e00a60cf6096
Yevhenii Nadtochii <yevhenii.nadtochii@teamdev.com>
2023-08-10 16:15:17 +0300

    Migrate `ThrowableStackGetterTest` to Kotlin

--- flogger/flogger-api-tests/src/test/kotlin/com/google/common/flogger/util/ThrowableStackGetterSpec.kt
+++ flogger/flogger-api-tests/src/test/kotlin/com/google/common/flogger/util/ThrowableStackGetterSpec.kt
@@ -1,33 +1,36 @@
-///*
-// * Copyright (C) 2021 The Flogger Authors.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License")
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// * http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package com.google.common.flogger.util
-//
-//import org.junit.jupiter.api.Test
-//
-//
-//internal class ThrowableStackGetterSpec {
-//
-//  @Test
-//  fun testCallerOf() {
-//    StackGetterTestSpec.runTestCallerOf(new ThrowableStackGetter())
-//  }
-//
-//  @Test
-//  fun testCallerOfBadOffset() {
-//    StackGetterTestSpec.runTestCallerOfBadOffset(new ThrowableStackGetter())
-//  }
-//}
+/*
+ * Copyright (C) 2021 The Flogger Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.flogger.util
+
+import com.google.common.flogger.util.StackGetterTestEnv.runTestCallerOf
+import com.google.common.flogger.util.StackGetterTestEnv.runTestCallerOfBadOffset
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("`ThrowableStackGetter` should")
+internal class ThrowableStackGetterSpec {
+
+    @Test
+    fun `find the stack trace element of the immediate caller of the specified class`() {
+        runTestCallerOf(ThrowableStackGetter())
+    }
+
+    @Test
+    fun `return 'null' due to wrong skip count`() {
+        runTestCallerOfBadOffset(ThrowableStackGetter())
+    }
+}
