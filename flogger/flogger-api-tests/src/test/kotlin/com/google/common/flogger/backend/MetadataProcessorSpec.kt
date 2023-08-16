2cc4e88a9f51ef959522fecb6dcd3494492a07b6
Yevhenii Nadtochii <yevhenii.nadtochii@teamdev.com>
2023-08-16 18:54:45 +0300

    Group processor tests

--- flogger/flogger-api-tests/src/test/kotlin/com/google/common/flogger/backend/MetadataProcessorSpec.kt
+++ flogger/flogger-api-tests/src/test/kotlin/com/google/common/flogger/backend/MetadataProcessorSpec.kt
@@ -37,6 +37,7 @@ import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.ints.shouldBeLessThanOrEqual
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 /**
@@ -55,80 +56,88 @@ internal abstract class MetadataProcessorSpec(private val factory: ProcessorFact
         private val REP_2 = repeated("R2", String::class.java)
     }
 
-    @Test
-    fun `combine scope and log site metadata with singleton keys`() {
-        val scope = FakeMetadata().add(KEY_1, "one").add(KEY_2, "two")
-        val logged = FakeMetadata().add(KEY_3, "three")
-        val metadata = factory.processorFor(scope, logged)
-        entries(metadata) shouldContainExactly listOf("K1=one", "K2=two", "K3=three")
-        metadata.keySet() shouldContainExactly setOf(KEY_1, KEY_2, KEY_3)
-        metadata.getSingleValue(KEY_1) shouldBe "one"
-        metadata.getSingleValue(KEY_3) shouldBe "three"
-    }
+    @Nested inner class
+    `combine scope and log site metadata` {
 
-    @Test
-    fun `combine scope and log site metadata with mixed keys`() {
-        val scope = FakeMetadata()
-            .add(REP_1, "first")
-            .add(KEY_1, "single")
-            .add(REP_1, "second")
-        val logged = FakeMetadata().add(REP_1, "third")
-        val metadata = factory.processorFor(scope, logged)
-        entries(metadata) shouldContainExactly listOf("R1=[first, second, third]", "K1=single")
-        metadata.keySet() shouldContainExactly setOf(REP_1, KEY_1)
-        metadata.getSingleValue(KEY_1) shouldBe "single"
-        shouldThrow<IllegalArgumentException> {
-            metadata.getSingleValue(REP_1)
+        @Test
+        fun `with singleton keys`() {
+            val scope = FakeMetadata().add(KEY_1, "one").add(KEY_2, "two")
+            val logged = FakeMetadata().add(KEY_3, "three")
+            val metadata = factory.processorFor(scope, logged)
+            entries(metadata) shouldContainExactly listOf("K1=one", "K2=two", "K3=three")
+            metadata.keySet() shouldContainExactly setOf(KEY_1, KEY_2, KEY_3)
+            metadata.getSingleValue(KEY_1) shouldBe "one"
+            metadata.getSingleValue(KEY_3) shouldBe "three"
+        }
+
+        @Test
+        fun `with mixed keys`() {
+            val scope = FakeMetadata()
+                .add(REP_1, "first")
+                .add(KEY_1, "single")
+                .add(REP_1, "second")
+            val logged = FakeMetadata().add(REP_1, "third")
+            val metadata = factory.processorFor(scope, logged)
+            entries(metadata) shouldContainExactly listOf("R1=[first, second, third]", "K1=single")
+            metadata.keySet() shouldContainExactly setOf(REP_1, KEY_1)
+            metadata.getSingleValue(KEY_1) shouldBe "single"
+            shouldThrow<IllegalArgumentException> {
+                metadata.getSingleValue(REP_1)
+            }
+        }
+
+        @Test
+        fun `with overriding and duplicating keys`() {
+            val scope = FakeMetadata()
+                .add(KEY_1, "original")
+                .add(REP_1, "r1-1")
+                .add(REP_2, "r2-1")
+                .add(REP_1, "r1-2")
+            val logged = FakeMetadata()
+                .add(REP_2, "r2-2")
+                .add(KEY_2, "value")
+                .add(REP_2, "r2-1") // Duplicate.
+                .add(KEY_1, "override") // Override.
+            val metadata = factory.processorFor(scope, logged)
+            val expected = listOf("K1=override", "R1=[r1-1, r1-2]", "R2=[r2-1, r2-2, r2-1]", "K2=value")
+            entries(metadata) shouldContainExactly expected
+            metadata.keySet() shouldContainExactly setOf(KEY_1, REP_1, REP_2, KEY_2)
         }
     }
 
-    @Test
-    fun `combine scope and log site metadata with overriding and duplicating keys`() {
-        val scope = FakeMetadata()
-            .add(KEY_1, "original")
-            .add(REP_1, "r1-1")
-            .add(REP_2, "r2-1")
-            .add(REP_1, "r1-2")
-        val logged = FakeMetadata()
-            .add(REP_2, "r2-2")
-            .add(KEY_2, "value")
-            .add(REP_2, "r2-1") // Duplicate.
-            .add(KEY_1, "override") // Override.
-        val metadata = factory.processorFor(scope, logged)
-        val expected = listOf("K1=override", "R1=[r1-1, r1-2]", "R2=[r2-1, r2-2, r2-1]", "K2=value")
-        entries(metadata) shouldContainExactly expected
-        metadata.keySet() shouldContainExactly setOf(KEY_1, REP_1, REP_2, KEY_2)
-    }
+    @Nested inner class
+    `process the maximum number of entries` {
 
-    @Test
-    fun `process the maximum number of entries with shareable keys`() {
-        val scope = FakeMetadata()
-        repeat(28) { i -> // 28 is a max number of entries for the lightweight processor.
-            val key = if (i and 1 == 0) REP_1 else REP_2
-            scope.add(key, "v$i")
+        @Test
+        fun `with shareable keys`() {
+            val scope = FakeMetadata()
+            repeat(28) { i -> // 28 is a max number of entries for the lightweight processor.
+                val key = if (i and 1 == 0) REP_1 else REP_2
+                scope.add(key, "v$i")
+            }
+            val metadata = factory.processorFor(scope, Metadata.empty())
+            entries(metadata) shouldContainExactly listOf(
+                "R1=[v0, v2, v4, v6, v8, v10, v12, v14, v16, v18, v20, v22, v24, v26]",
+                "R2=[v1, v3, v5, v7, v9, v11, v13, v15, v17, v19, v21, v23, v25, v27]"
+            )
         }
-        val metadata = factory.processorFor(scope, Metadata.empty())
-        entries(metadata) shouldContainExactly listOf(
-            "R1=[v0, v2, v4, v6, v8, v10, v12, v14, v16, v18, v20, v22, v24, v26]",
-            "R2=[v1, v3, v5, v7, v9, v11, v13, v15, v17, v19, v21, v23, v25, v27]"
-        )
-    }
 
-    @Test
-    fun `process the maximum number of entries with distinct keys`() {
-        val scope = FakeMetadata()
-        repeat(28) { i -> // 28 is a max number of entries for the lightweight processor.
-            val key = single("K$i", String::class.java)
-            val value = "v$i"
-            scope.add(key, value)
+        @Test
+        fun `with distinct keys`() {
+            val scope = FakeMetadata()
+            repeat(28) { i -> // 28 is a max number of entries for the lightweight processor.
+                val key = single("K$i", String::class.java)
+                val value = "v$i"
+                scope.add(key, value)
+            }
+            val metadata = factory.processorFor(scope, Metadata.empty())
+            entries(metadata) shouldContainExactly listOf(
+                "K0=v0", "K1=v1", "K2=v2", "K3=v3", "K4=v4", "K5=v5", "K6=v6",
+                "K7=v7", "K8=v8", "K9=v9", "K10=v10", "K11=v11", "K12=v12", "K13=v13",
+                "K14=v14", "K15=v15", "K16=v16", "K17=v17", "K18=v18", "K19=v19", "K20=v20",
+                "K21=v21", "K22=v22", "K23=v23", "K24=v24", "K25=v25", "K26=v26", "K27=v27"
+            )
         }
-        val metadata = factory.processorFor(scope, Metadata.empty())
-        entries(metadata) shouldContainExactly listOf(
-            "K0=v0", "K1=v1", "K2=v2", "K3=v3", "K4=v4", "K5=v5", "K6=v6",
-            "K7=v7", "K8=v8", "K9=v9", "K10=v10", "K11=v11", "K12=v12", "K13=v13",
-            "K14=v14", "K15=v15", "K16=v16", "K17=v17", "K18=v18", "K19=v19", "K20=v20",
-            "K21=v21", "K22=v22", "K23=v23", "K24=v24", "K25=v25", "K26=v26", "K27=v27"
-        )
     }
 
     @Test
