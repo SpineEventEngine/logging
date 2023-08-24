ca46c49ed1de5a7916fb6663260f05a729d8b7e8
Yevhenii Nadtochii <yevhenii.nadtochii@teamdev.com>
2023-08-24 18:31:18 +0300

    Rebase on the latest `master`

--- flogger/flogger-system-backend/build.gradle.kts
+++ flogger/flogger-system-backend/build.gradle.kts
@@ -24,39 +24,31 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import BuildSettings.javaVersion
 import io.spine.internal.dependency.AutoService
-import io.spine.internal.dependency.CheckerFramework
-import io.spine.internal.dependency.ErrorProne
-import io.spine.internal.dependency.Truth
-import io.spine.internal.gradle.report.license.LicenseReporter
+import net.ltgt.gradle.errorprone.errorprone
 
 plugins {
-    `java-library`
+    `jvm-module`
 }
-
-LicenseReporter.generateReportIn(project)
 
 dependencies {
     implementation(project(":flogger-api"))
-    implementation(CheckerFramework.annotations)
-    ErrorProne.annotations.forEach { implementation(it) }
-
     testImplementation(project(":flogger-testing"))
-    testImplementation("org.mockito:mockito-core:4.11.0")
-    Truth.libs.forEach { testImplementation(it) }
-
-    testImplementation(AutoService.annotations)
+    testImplementation(AutoService.processor)
     testAnnotationProcessor(AutoService.processor)
 }
 
 java {
-    toolchain.languageVersion.set(javaVersion)
-}
 
-tasks {
-    named<Javadoc>("javadoc") {
-        // Produces a lot of formatting/linkage errors.
-        enabled = false
+    /**
+     * Disables Java linters until main sources are migrated to Kotlin.
+     *
+     * As for now, they produce a lot of errors/warnings to original Flogger code,
+     * failing the build.
+     */
+    tasks {
+        named("checkstyleMain") { enabled = false }
+        named("pmdMain") { enabled = false }
+        compileJava { options.errorprone.isEnabled.set(false) }
     }
 }
