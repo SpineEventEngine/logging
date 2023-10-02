995d37459792577037741847dd2f183e2f6ea5f2
Yevhenii Nadtochii <yevhenii.nadtochii@teamdev.com>
2023-10-02 14:54:47 +0300

    Address warnings

--- flogger/flogger-platform-generator/src/main/java/io/spine/logging/backend/generator/PlatformProviderGenerator.java
+++ flogger/flogger-platform-generator/src/main/java/io/spine/logging/backend/generator/PlatformProviderGenerator.java
@@ -26,6 +26,19 @@
 
 package io.spine.logging.backend.generator;
 
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Type;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+
+import static java.nio.file.Files.newOutputStream;
 import static org.objectweb.asm.Opcodes.ACC_FINAL;
 import static org.objectweb.asm.Opcodes.ACC_PRIVATE;
 import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
@@ -44,156 +57,195 @@ import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
 import static org.objectweb.asm.Opcodes.RETURN;
 import static org.objectweb.asm.Opcodes.V1_6;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.jar.JarOutputStream;
-import java.util.zip.ZipEntry;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Label;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Type;
-
 /**
- * Class that generates a PlatformProvider class for creating instances of Platform, for use in
- * Platform discovery.
+ * Generates {@code PlatformProvider} class from scratch.
  *
- * <p>This generator is necessary in order to create a class that explicitly references the actual
- * platform implementation classes without having them visible as a build dependency. If the code
- * were compiled with javac that wouldn't work, because javac needs to observe the classes on its
- * classpath. By resorting to manually generating the class file, we can work around the limitation,
- * and avoid the dependency on platform implementations while still keeping an explicit reference to
- * the classes. The advantage of this approach is that tools that operate on bytecode (e.g.
- * proguard) observe the dependency correctly, which is not the case when reflection is used to look
- * up classes.
+ * <p>The generated class discovers {@code io.spine.logging.flogger.backend.Platform}
+ * implementations, and creates their instances.
+ *
+ * <p>This generator is necessary to create a class, which explicitly
+ * references the actual platform implementation classes without having
+ * them visible as a build dependency.
+ *
+ * <p>If the code were compiled with {@code javac} that wouldn't work,
+ * because {@code javac} needs to observe the classes on its classpath.
+ * By resorting to manually generating file, we can work around the limitation
+ * and avoid the dependency on platform implementations while still keeping
+ * an explicit reference to the classes.
+ *
+ * <p>The advantage of this approach is that tools that operate on bytecode
+ * (e.g., proguard) observe the dependency correctly, which is not the case
+ * when reflection is used to look up classes.
  */
+@SuppressWarnings("DuplicateStringLiteralInspection") // Uses literals with FQN.
 public final class PlatformProviderGenerator {
-  private static final String[] PLATFORM_CLASSES =
-      new String[] {
-        "Lio/spine/logging/flogger/backend/system/DefaultPlatform;",
-      };
 
-  public static void main(String[] args) throws IOException {
-    // Create the class.
-    ClassWriter classWriter = new ClassWriter(0);
-    classWriter.visit(
-        V1_6,
-        ACC_PUBLIC + ACC_FINAL + ACC_SUPER,
-        "io/spine/logging/flogger/backend/PlatformProvider",
-        null,
-        "java/lang/Object",
-        null);
+    private static final String[] PLATFORM_CLASSES = {
+            "Lio/spine/logging/flogger/backend/system/DefaultPlatform;",
+    };
 
-    // Create the no-op constructor.
-    MethodVisitor methodVisitor = classWriter.visitMethod(ACC_PRIVATE, "<init>", "()V", null, null);
-    methodVisitor.visitCode();
-    methodVisitor.visitVarInsn(ALOAD, 0);
-    methodVisitor.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
-    methodVisitor.visitInsn(RETURN);
-    methodVisitor.visitMaxs(1, 1);
-    methodVisitor.visitEnd();
-
-    // Create the static getter method.
-    methodVisitor =
-        classWriter.visitMethod(
-            ACC_PUBLIC + ACC_STATIC,
-            "getPlatform",
-            "()Lio/spine/logging/flogger/backend/Platform;",
-            null,
-            null);
-    // Try the different platforms.
-    for (String platformClass : PLATFORM_CLASSES) {
-      tryBlockForPlatform(methodVisitor, platformClass);
+    /**
+     * Prevents instantiation of this utility class.
+     */
+    private PlatformProviderGenerator() {
     }
-    // Return null if no platform is found.
-    methodVisitor.visitInsn(ACONST_NULL);
-    methodVisitor.visitInsn(ARETURN);
-    methodVisitor.visitMaxs(2, 1);
-    methodVisitor.visitEnd();
 
-    // Finish creating the class.
-    classWriter.visitEnd();
+    public static void main(String[] args) throws IOException {
 
-    // Write the class to the output file.
-    Path path = Paths.get(args[0]);
-    Files.createDirectories(path.getParent());
-    try (JarOutputStream jar =
-        new JarOutputStream(Files.newOutputStream(path, StandardOpenOption.CREATE_NEW))) {
-      ZipEntry entry = new ZipEntry("io/spine/logging/flogger/backend/PlatformProvider.class");
-      entry.setTime(0); // clear timestamp to ensure JAR is deterministic for cache during builds.
-      jar.putNextEntry(entry);
-      jar.write(classWriter.toByteArray());
-      jar.closeEntry();
+        // Create the class.
+        var classWriter = new ClassWriter(0);
+        classWriter.visit(
+                V1_6,
+                ACC_PUBLIC + ACC_FINAL + ACC_SUPER,
+                "io/spine/logging/flogger/backend/PlatformProvider",
+                null,
+                "java/lang/Object",
+                null)
+        ;
+
+        // Create the no-op constructor.
+        var methodVisitor = classWriter.visitMethod(ACC_PRIVATE, "<init>", "()V", null, null);
+        methodVisitor.visitCode();
+        methodVisitor.visitVarInsn(ALOAD, 0);
+        methodVisitor.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+        methodVisitor.visitInsn(RETURN);
+        methodVisitor.visitMaxs(1, 1);
+        methodVisitor.visitEnd();
+
+        // Create the static getter method.
+        methodVisitor = classWriter.visitMethod(
+                ACC_PUBLIC + ACC_STATIC,
+                "getPlatform",
+                "()Lio/spine/logging/flogger/backend/Platform;",
+                null,
+                null
+        );
+
+        // Try different platforms.
+        for (var platformClass : PLATFORM_CLASSES) {
+            tryBlockForPlatform(methodVisitor, platformClass);
+        }
+
+        // Return `null` if no platform is found.
+        methodVisitor.visitInsn(ACONST_NULL);
+        methodVisitor.visitInsn(ARETURN);
+        methodVisitor.visitMaxs(2, 1);
+        methodVisitor.visitEnd();
+
+        // Finish creating the class.
+        classWriter.visitEnd();
+
+        // Write the class to the output file.
+        var path = Paths.get(args[0]);
+        Files.createDirectories(path.getParent());
+        try (var jar = new JarOutputStream(newOutputStream(path, StandardOpenOption.CREATE_NEW))) {
+            var entry = new ZipEntry("io/spine/logging/flogger/backend/PlatformProvider.class");
+            // Clear timestamp to ensure JAR is deterministic for build cache.
+            entry.setTime(0);
+            jar.putNextEntry(entry);
+            jar.write(classWriter.toByteArray());
+            jar.closeEntry();
+        }
     }
-  }
 
-  private static void tryBlockForPlatform(MethodVisitor methodVisitor, String platformType) {
-    methodVisitor.visitCode();
+    /**
+     * Generates the enveloping try-catch block.
+     *
+     * <p>Here is an example of the generated code:
+     *
+     * <pre>
+     * try {
+     *     return (Platform) PlatformClass.class.getDeclaredConstructor().newInstance();
+     * } catch(NoClassDefFoundError | IllegalAccessException | InstantiationException | InvocationTargetException | NoSuchMethodException e) {
+     *     return null;
+     * }
+     * </pre>
+     *
+     * <p>Please note that the exception types need to be listed explicitly
+     * rather than using {@link ReflectiveOperationException}) because
+     * that parent exception type is not available on Android until
+     * API level 19 (Android 4.4, 2013).
+     */
+    private static void tryBlockForPlatform(MethodVisitor methodVisitor, String platformType) {
+        methodVisitor.visitCode();
 
-    // Generate the enveloping try/catch block:
-    //
-    //   try {
-    //     ...
-    //   } catch (NoClassDefFoundError | IllegalAccessException | InstantiationException
-    //       | InvocationTargetException | NoSuchMethodException e) {
-    //     ...
-    //   }
-    //
-    // Note that the exception types need to be listed explicitly (rather than using
-    // java.lang.ReflectiveOperationException) because that parent exception type isn't available
-    // on Android until API level 19.
-    Label startLabel = new Label();
-    Label endLabel = new Label();
-    Label handlerLabel = new Label();
-    methodVisitor.visitTryCatchBlock(
-        startLabel, endLabel, handlerLabel, "java/lang/NoClassDefFoundError");
-    methodVisitor.visitTryCatchBlock(
-        startLabel, endLabel, handlerLabel, "java/lang/IllegalAccessException");
-    methodVisitor.visitTryCatchBlock(
-        startLabel, endLabel, handlerLabel, "java/lang/InstantiationException");
-    methodVisitor.visitTryCatchBlock(
-        startLabel, endLabel, handlerLabel, "java/lang/reflect/InvocationTargetException");
-    methodVisitor.visitTryCatchBlock(
-        startLabel, endLabel, handlerLabel, "java/lang/NoSuchMethodException");
-    methodVisitor.visitLabel(startLabel);
+        /*
+        Generate the enveloping try-catch block:
 
-    // Generate the actual reflective constructor call inside the try block:
-    //
-    //   return (Platform) PlatformClass.class.getDeclaredConstructor().newInstance();
-    //
-    // Note that the constructor call happens reflectively to make sure that the platform class
-    // isn't loaded until actually executing this instruction. That is important because an
-    // earlier class load could happen outside of the try/catch block where we are explicitly
-    // handling the case of the class not being present.
-    methodVisitor.visitLdcInsn(Type.getType(platformType));
-    methodVisitor.visitInsn(ICONST_0);
-    methodVisitor.visitTypeInsn(ANEWARRAY, "java/lang/Class");
-    methodVisitor.visitMethodInsn(
-        INVOKEVIRTUAL,
-        "java/lang/Class",
-        "getDeclaredConstructor",
-        "([Ljava/lang/Class;)Ljava/lang/reflect/Constructor;",
-        false);
-    methodVisitor.visitInsn(ICONST_0);
-    methodVisitor.visitTypeInsn(ANEWARRAY, "java/lang/Object");
-    methodVisitor.visitMethodInsn(
-        INVOKEVIRTUAL,
-        "java/lang/reflect/Constructor",
-        "newInstance",
-        "([Ljava/lang/Object;)Ljava/lang/Object;",
-        false);
-    methodVisitor.visitTypeInsn(CHECKCAST, "io/spine/logging/flogger/backend/Platform");
-    methodVisitor.visitLabel(endLabel);
-    methodVisitor.visitInsn(ARETURN);
+        ```
+        try {
+          ...
+        } catch (NoClassDefFoundError | IllegalAccessException | InstantiationException
+            | InvocationTargetException | NoSuchMethodException e) {
+          ...
+        }
+        ```
 
-    // Generate the catch block of the overall try/catch. The catch block is actually just empty,
-    // but Java does require the catch handler to have at least a frame in it to declare the
-    // exception variable that is available within the catch block scope:
-    // https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-3.html#jvms-3.12
-    methodVisitor.visitLabel(handlerLabel);
-    methodVisitor.visitFrame(F_SAME1, 0, null, 1, new Object[] {"java/lang/Throwable"});
-    methodVisitor.visitVarInsn(ASTORE, 0);
-  }
+         */
+        var startLabel = new Label();
+        var endLabel = new Label();
+        var handlerLabel = new Label();
+        methodVisitor.visitTryCatchBlock(
+                startLabel, endLabel, handlerLabel, "java/lang/NoClassDefFoundError");
+        methodVisitor.visitTryCatchBlock(
+                startLabel, endLabel, handlerLabel, "java/lang/IllegalAccessException");
+        methodVisitor.visitTryCatchBlock(
+                startLabel, endLabel, handlerLabel, "java/lang/InstantiationException");
+        methodVisitor.visitTryCatchBlock(
+                startLabel, endLabel, handlerLabel, "java/lang/reflect/InvocationTargetException");
+        methodVisitor.visitTryCatchBlock(
+                startLabel, endLabel, handlerLabel, "java/lang/NoSuchMethodException");
+        methodVisitor.visitLabel(startLabel);
+
+
+
+        /*
+        Generate the actual reflective constructor call inside the try block:
+
+        ```
+        return (Platform) PlatformClass.class.getDeclaredConstructor().newInstance();
+        ```
+
+        Note that the constructor call happens reflectively to make sure
+        that the platform class isn't loaded until actually executing
+        this instruction.
+
+        That is important because an earlier class load could happen outside
+        the try/catch block where we are explicitly handling the case of
+        the class not being present.
+         */
+        methodVisitor.visitLdcInsn(Type.getType(platformType));
+        methodVisitor.visitInsn(ICONST_0);
+        methodVisitor.visitTypeInsn(ANEWARRAY, "java/lang/Class");
+        methodVisitor.visitMethodInsn(
+                INVOKEVIRTUAL,
+                "java/lang/Class",
+                "getDeclaredConstructor",
+                "([Ljava/lang/Class;)Ljava/lang/reflect/Constructor;",
+                false);
+        methodVisitor.visitInsn(ICONST_0);
+        methodVisitor.visitTypeInsn(ANEWARRAY, "java/lang/Object");
+        methodVisitor.visitMethodInsn(
+                INVOKEVIRTUAL,
+                "java/lang/reflect/Constructor",
+                "newInstance",
+                "([Ljava/lang/Object;)Ljava/lang/Object;",
+                false);
+        methodVisitor.visitTypeInsn(CHECKCAST, "io/spine/logging/flogger/backend/Platform");
+        methodVisitor.visitLabel(endLabel);
+        methodVisitor.visitInsn(ARETURN);
+
+        /*
+        Generate the catch block of the overall try/catch.
+
+        The catch block is actually just empty, but Java does require the catch
+        handler to have at least a frame in it to declare the exception variable
+        that is available within the catch block scope.
+
+        https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-3.html#jvms-3.12
+         */
+        methodVisitor.visitLabel(handlerLabel);
+        methodVisitor.visitFrame(F_SAME1, 0, null, 1, new Object[]{"java/lang/Throwable"});
+        methodVisitor.visitVarInsn(ASTORE, 0);
+    }
 }
