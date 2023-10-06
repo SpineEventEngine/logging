cb2e00eadb992899e39f1e1173b3e069f139a442
Yevhenii Nadtochii <yevhenii.nadtochii@teamdev.com>
2023-10-06 18:25:35 +0300

    Move `probe-backend` to other backends

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,13 +36,13 @@ rootProject.name = "spine-logging"
 include(
     "logging",
     "logging-context",
-    "logging-probe-backend",
     "testutil-logging",
 )
 
 includeBackend(
     "logging-log4j2-backend",
     "logging-jul-backend",
+    "logging-probe-backend",
 )
 
 includeContext(
