9273c43ab524fbf4c953ff3709ce22547942ae58
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-11-26 13:34:27 +0000

    Update the versions of dependencies taken from the latest `config`.

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Kotlin.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Kotlin.kt
@@ -35,13 +35,13 @@ object Kotlin {
      * When changing the version, also change the version used in the `buildSrc/build.gradle.kts`.
      */
     @Suppress("MemberVisibilityCanBePrivate") // used directly from outside
-    const val version = "1.9.10"
+    const val version = "1.9.20"
 
     /**
      * The version of the JetBrains annotations library, which is a transitive
      * dependency for us via Kotlin libraries.
      *
-     * https://github.com/JetBrains/java-annotations
+     * @see <a href="https://github.com/JetBrains/java-annotations">Java Annotations</a>
      */
     private const val annotationsVersion = "24.0.1"
 
