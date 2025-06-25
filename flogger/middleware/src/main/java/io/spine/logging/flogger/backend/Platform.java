a4f7fea51c70a6b8874e4c83e10153576f82c5db
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-06-25 16:38:01 +0100

    Simplify method return

--- flogger/middleware/src/main/java/io/spine/logging/flogger/backend/Platform.java
+++ flogger/middleware/src/main/java/io/spine/logging/flogger/backend/Platform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019, The Flogger Authors; 2025, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -270,9 +270,6 @@ public abstract class Platform {
       checkNotNull(loggerName);
       var provider = getContextDataProvider();
       var result = provider.getMappedLevel(loggerName);
-      if (result == null) {
-        return null;
-      }
       return result;
   }
 
