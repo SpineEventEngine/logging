a9df070b4dce23275a33610ffd7332a08407eca4
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-07-14 23:26:57 +0100

    Fix doc layout

--- jvm/middleware/src/main/kotlin/io/spine/logging/jvm/parser/MessageBuilder.kt
+++ jvm/middleware/src/main/kotlin/io/spine/logging/jvm/parser/MessageBuilder.kt
@@ -31,9 +31,10 @@ import io.spine.logging.jvm.parameter.Parameter
 import io.spine.logging.jvm.util.Checks
 
 /**
- * A builder which is used during message parsing to create a message object which encapsulates
- * all the formatting requirements of a log message. One message builder is created for each log
- * message that's parsed.
+ * A builder which is used during message parsing to create a message object which
+ * encapsulates all the formatting requirements of a log message.
+ *
+ * One message builder is created for each log message that's parsed.
  *
  * @param T The message type being built.
  *
@@ -57,25 +58,31 @@ public abstract class MessageBuilder<T>(context: TemplateContext) {
      */
     private var maxIndex: Int = -1
 
-    /** Returns the parser used to process the log format message in this builder. */
+    /**
+     * Returns the parser used to process the log format message in this builder.
+     */
     public val parser: MessageParser
         get() = context.parser
 
-    /** Returns the log format message to be parsed by this builder. */
+    /**
+     * Returns the log format message to be parsed by this builder.
+     */
     public val message: String
         get() = context.message
 
     /**
-     * Returns the expected number of arguments to be formatted by this message. This is only valid
-     * once parsing has completed successfully.
+     * Returns the expected number of arguments to be formatted by this message.
+     *
+     * This is only valid once parsing has completed successfully.
      */
     public val expectedArgumentCount: Int
         get() = maxIndex + 1
 
 
     /**
-     * Called by parser implementations to signify that the parsing of the next parameter is
-     * complete.
+     * Called by parser implementations to signify that the parsing of the next
+     * parameter is complete.
+     * 
      * This method will call [addParameterImpl] with exactly the same
      * arguments, but may also do additional work before or after that call.
      *
@@ -105,11 +112,11 @@ public abstract class MessageBuilder<T>(context: TemplateContext) {
      * characters from the log message and that each range will be higher that the previously
      * specified one.
      *
-     * @param termStart the index of the first character in the log message string that was parsed to
+     * @param termStart The index of the first character in the log message string that was parsed to
      *        form the given parameter.
-     * @param termEnd the index after the last character in the log message string that was parsed to
+     * @param termEnd The index after the last character in the log message string that was parsed to
      *        form the given parameter.
-     * @param param a parameter representing the format specified by the substring of the log message
+     * @param param A parameter representing the format specified by the substring of the log message
      *        in the range `[termStart, termEnd)`.
      */
     protected abstract fun addParameterImpl(termStart: Int, termEnd: Int, param: Parameter)
@@ -145,111 +152,3 @@ public abstract class MessageBuilder<T>(context: TemplateContext) {
         return buildImpl()
     }
 }
-//
-///**
-// * A builder which is used during message parsing to create a message object which encapsulates
-// * all the formatting requirements of a log message. One message builder is created for each log
-// * message that's parsed.
-// *
-// * @param T The message type being built.
-// *
-// * @see [Original Java code of Google Flogger](https://github.com/google/flogger/blob/cb9e836a897d36a78309ee8badf5cad4e6a2d3d8/api/src/main/java/com/google/common/flogger/parser/MessageBuilder.java)
-// * for historical context.
-// */
-//public abstract class MessageBuilder<T>(context: TemplateContext) {
-//
-//    private val context: TemplateContext = Checks.checkNotNull(context, "context")
-//
-//    // Mask of parameter indexes seen during parsing, used to determine if there are gaps in the
-//    // specified parameters (which is a parsing error).
-//    // This could be a long if we cared about tracking up to 64 parameters, but I suspect we don't.
-//    private var pmask: Int = 0
-//
-//    // The maximum argument index referenced by the formatted message (only valid after parsing).
-//    private var maxIndex: Int = -1
-//
-//    /** Returns the parser used to process the log format message in this builder. */
-//    public fun getParser(): MessageParser {
-//        return context.parser
-//    }
-//
-//    /** Returns the log format message to be parsed by this builder. */
-//    public fun getMessage(): String {
-//        return context.message
-//    }
-//
-//    /**
-//     * Returns the expected number of arguments to be formatted by this message. This is only valid
-//     * once parsing has completed successfully.
-//     */
-//    public fun getExpectedArgumentCount(): Int {
-//        return maxIndex + 1
-//    }
-//
-//    /**
-//     * Called by parser implementations to signify that the parsing of the next parameter is
-//     * complete.
-//     * This method will call [addParameterImpl] with exactly the same
-//     * arguments, but may also do additional work before or after that call.
-//     *
-//     * @param termStart the index of the first character in the log message string that was parsed to
-//     *        form the given parameter.
-//     * @param termEnd the index after the last character in the log message string that was parsed to
-//     *        form the given parameter.
-//     * @param param a parameter representing the format specified by the substring of the log message
-//     *        in the range `[termStart, termEnd)`.
-//     */
-//    @Suppress("MagicNumber")
-//    public fun addParameter(termStart: Int, termEnd: Int, param: Parameter) {
-//        // Set a bit in the parameter mask according to which parameter was referenced.
-//        // Shifting wraps, so we must do a check here.
-//        if (param.index < 32) {
-//            pmask = pmask or (1 shl param.index)
-//        }
-//        maxIndex = maxOf(maxIndex, param.index)
-//        addParameterImpl(termStart, termEnd, param)
-//    }
-//
-//    /**
-//     * Adds the specified parameter to the format instance currently being built. This method is to
-//     * signify that the parsing of the next parameter is complete.
-//     *
-//     * Note that each successive call to this method during parsing will specify a disjoint ranges of
-//     * characters from the log message and that each range will be higher that the previously
-//     * specified one.
-//     *
-//     * @param termStart the index of the first character in the log message string that was parsed to
-//     *        form the given parameter.
-//     * @param termEnd the index after the last character in the log message string that was parsed to
-//     *        form the given parameter.
-//     * @param param a parameter representing the format specified by the substring of the log message
-//     *        in the range `[termStart, termEnd)`.
-//     */
-//    protected abstract fun addParameterImpl(termStart: Int, termEnd: Int, param: Parameter)
-//
-//    /**
-//     * Returns the implementation-specific result of parsing the current log message.
-//     */
-//    protected abstract fun buildImpl(): T
-//
-//    /**
-//     * Builds a log message using the current message context.
-//     *
-//     * @return the implementation-specific result of parsing the current log message.
-//     */
-//    @Suppress("ImplicitDefaultLocale", "MagicNumber")
-//    public fun build(): T {
-//        getParser().parseImpl<T>(this)
-//        // There was a gap in the parameters if either:
-//        // 1) the mask had a gap, e.g., `..00110111`
-//        // 2) there were more than 32 parameters, and the mask was not full.
-//        // Gaps above the 32nd parameter are not detected.
-//        if ((pmask and (pmask + 1)) != 0 || (maxIndex > 31 && pmask != -1)) {
-//            val firstMissing = Integer.numberOfTrailingZeros(pmask.inv())
-//            throw ParseException.generic(
-//                String.format("unreferenced arguments [first missing index=%d]", firstMissing)
-//            )
-//        }
-//        return buildImpl()
-//    }
-//}
