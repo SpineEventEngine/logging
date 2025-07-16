df2751fdb6f373606bd4e6bed177876f452d8fec
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-07-16 13:50:20 +0100

    Fix missing leading star

--- jvm/middleware/src/main/kotlin/io/spine/logging/jvm/parser/DefaultBraceStyleMessageParser.kt
+++ jvm/middleware/src/main/kotlin/io/spine/logging/jvm/parser/DefaultBraceStyleMessageParser.kt
@@ -40,7 +40,7 @@ import io.spine.logging.jvm.parameter.BraceStyleParameter
  * supported however, this may change. Currently, an unescaped "{}" term in a log message will cause
  * a parse error, so adding support for it should not be an issue.
  *
- @see <a href="https://github.com/google/flogger/blob/cb9e836a897d36a78309ee8badf5cad4e6a2d3d8/api/src/main/java/com/google/common/flogger/parser/DefaultBraceStyleMessageParser.java">
+ * @see <a href="https://github.com/google/flogger/blob/cb9e836a897d36a78309ee8badf5cad4e6a2d3d8/api/src/main/java/com/google/common/flogger/parser/DefaultBraceStyleMessageParser.java">
  *     Original Java code of Google Flogger</a> for historical context.
  */
 public class DefaultBraceStyleMessageParser private constructor() : BraceStyleMessageParser() {
