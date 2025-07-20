22da5800392c36dbf655d6420409a3adb2bf94ed
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-07-20 13:18:05 +0100

    Eliminate `LazyArgs` utility class

--- jvm/middleware/src/main/kotlin/io/spine/logging/jvm/LazyArg.kt
+++ jvm/middleware/src/main/kotlin/io/spine/logging/jvm/LazyArg.kt
@@ -30,11 +30,11 @@ import io.spine.logging.jvm.util.Checks.checkNotNull
 import org.jspecify.annotations.Nullable
 
 /**
- * Functional interface for allowing lazily evaluated arguments to be supplied to Flogger.
+ * A functional interface for allowing lazily evaluated arguments to be supplied to a logger.
  *
  * This allows callers to defer argument evaluation efficiently when:
  * - Doing "fine" logging that's normally disabled
- * - Applying rate limiting to log statements
+ * - Applying rate limiting to log statements for better performance control.
  *
  * @see <a href="https://github.com/google/flogger/blob/cb9e836a897d36a78309ee8badf5cad4e6a2d3d8/api/src/main/java/com/google/common/flogger/LazyArg.java">
  *     Original Java code of Google Flogger</a> for historical context.
@@ -44,9 +44,9 @@ public fun interface LazyArg<T> {
     /**
      * Computes a value to use as a log argument.
      *
-     * This method is invoked once the Flogger library has determined that logging will occur,
-     * and the returned value is used in place of the `LazyArg` instance that was passed into
-     * the log statement.
+     * This method is invoked once the Logging library has determined that logging will
+     * occur, and the returned value is used in place of the `LazyArg` instance that was
+     * passed into the log statement.
      */
     public fun evaluate(): @Nullable T
 
@@ -56,24 +56,25 @@ public fun interface LazyArg<T> {
          * Coerces a lambda expression or method reference to return a lazily evaluated
          * logging argument.
          *
-         * Pass in a compatible, no-argument, lambda expression or method reference to have it
-         * evaluated only when logging will actually occur.
+         * Pass in a compatible, no-argument, lambda expression or method reference to have
+         * it evaluated only when logging will actually occur.
          *
          * ```kotlin
-         * logger.atFine().log("value=%s", LazyArg.lazy { doExpensive() })
-         * logger.atWarning().atMostEvery(5, MINUTES).log("value=%s", LazyArg.lazy(stats::create))
+         * logger.atFine().log("value=%s", lazy { doExpensive() })
+         * logger.atWarning().atMostEvery(5, MINUTES).log("value=%s", lazy(stats::create))
          * ```
          *
-         * Evaluation of lazy arguments occurs at most once, and always in the same thread from
-         * which the logging call was made.
+         * Evaluation of lazy arguments occurs at most once, and always in the same thread
+         * from which the logging call was made.
          *
-         * Note also that it is almost never suitable to make a `toString()` call "lazy" using
-         * this mechanism and, in general, explicitly calling `toString()` on arguments which
-         * are being logged is an error as it precludes the ability to log an argument
+         * Note also that it is almost never suitable to make a `toString()` call "lazy"
+         * using this mechanism and, in general, explicitly calling `toString()` on arguments
+         * which are being logged is an error as it precludes the ability to log an argument
          * structurally.
          */
         @JvmStatic
         public fun <T> lazy(lambdaOrMethodReference: LazyArg<T>): LazyArg<T> {
+            
             /**
              * This method is essentially a coercing cast for the functional interface to give
              * the compiler a target type to convert a lambda expression or method reference into.
