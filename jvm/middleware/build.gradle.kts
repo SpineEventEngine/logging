fa58e669ee196a19b8f3d352a86142dec7dc0a96
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-07-24 13:38:40 +0100

    Add KotlinX DateTime dependency object

--- jvm/middleware/build.gradle.kts
+++ jvm/middleware/build.gradle.kts
@@ -24,6 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import io.spine.dependency.kotlinx.DateTime
 import io.spine.dependency.local.Base
 import io.spine.dependency.local.Reflect
 import io.spine.gradle.java.disableLinters
@@ -34,6 +35,7 @@ plugins {
 }
 
 dependencies {
+    implementation(DateTime.lib)
     implementation(Base.annotations)
     implementation(Reflect.lib)
     implementation(project(":platform-generator", configuration = "generatedPlatformProvider"))
