4e7c84d54de151572771c113f4d9ab275985f831
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-08-09 09:52:29 +0100

    Remove redundant `null` handling

--- logging/src/jvmMain/kotlin/io/spine/logging/jvm/AbstractLogger.kt
+++ logging/src/jvmMain/kotlin/io/spine/logging/jvm/AbstractLogger.kt
@@ -239,7 +239,7 @@ public abstract class AbstractLogger<API : MiddlemanApi<API>> protected construc
         val out = buildString {
             append(formatTimestampIso8601(data))
             append(": logging error [")
-            append(data.logSite?.toString() ?: "UNKNOWN")
+            append(data.logSite.toString())
             append("]: ")
             append(message)
         }
