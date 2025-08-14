fb202aa62a80a841c4a55b35c207665fae6e0a38
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-08-14 14:11:14 +0100

    Optimise imports

--- logging/src/jvmMain/kotlin/io/spine/logging/KeyPart.kt
+++ logging/src/jvmMain/kotlin/io/spine/logging/KeyPart.kt
@@ -26,7 +26,6 @@
 
 package io.spine.logging
 
-import io.spine.logging.LoggingScope
 import java.lang.ref.ReferenceQueue
 import java.lang.ref.WeakReference
 import java.util.concurrent.ConcurrentLinkedQueue
