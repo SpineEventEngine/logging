d977b64a3dcb40813efb4ab40c763c5f08bcc259
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-08-15 12:24:38 +0100

    Use our `@VisibleForTesting`

--- logging/src/jvmMain/kotlin/io/spine/logging/jvm/MetadataKey.kt
+++ logging/src/jvmMain/kotlin/io/spine/logging/jvm/MetadataKey.kt
@@ -26,10 +26,11 @@
 
 package io.spine.logging.jvm
 
+import io.spine.annotation.VisibleForTesting
 import io.spine.logging.KeyValueHandler
+import io.spine.logging.jvm.MetadataKey.Companion.single
 import io.spine.logging.jvm.backend.Platform
 import io.spine.logging.jvm.util.Checks
-import org.jetbrains.annotations.VisibleForTesting
 
 /**
  * A key for logging semi-structured metadata values.
