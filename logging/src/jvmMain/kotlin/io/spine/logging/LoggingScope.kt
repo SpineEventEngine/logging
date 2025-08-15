7d057845070b3e63bbf7c6b5ece49c740ad395a7
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-08-15 12:26:51 +0100

    Add year to (c) statements

--- logging/src/jvmMain/kotlin/io/spine/logging/LoggingScope.kt
+++ logging/src/jvmMain/kotlin/io/spine/logging/LoggingScope.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020, The Flogger Authors; TeamDev. All rights reserved.
+ * Copyright 2020, The Flogger Authors; 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
