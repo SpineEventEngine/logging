77721c4f53e5bf79552812b3890a95f51ff07a98
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-09-02 19:25:31 +0100

    Make `RecursionDepth` platform-neutral

---
+++ logging/src/commonMain/kotlin/io/spine/logging/util/RecursionDepth.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2025, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.logging.util
+
+/**
+ * A platform-neutral API for tracking recursion depth of logging operations.
+ *
+ * Use `Platform.getCurrentRecursionDepth()` to query the current depth from outside
+ * of the core logging internals.
+ *
+ * ### API Note
+ *
+ * This class is an internal detail and must not be used outside the core of the Logging library.
+ */
+public expect class RecursionDepth() : AutoCloseable {
+
+    public companion object {
+
+        /**
+         * Do not call this method directly, use `Platform.getCurrentRecursionDepth()`.
+         */
+        @JvmStatic
+        public fun getCurrentDepth(): Int
+
+        /**
+         * Do not call this method directly, use `Platform.getCurrentRecursionDepth()`.
+         */
+        @JvmStatic
+        public fun enterLogStatement(): RecursionDepth
+    }
+
+    /** Do not call this method directly, use `Platform.getCurrentRecursionDepth()`. */
+    public fun getValue(): Int
+
+    override fun close()
+}
