fbff48a9252606cbf4a0780daa5198fcb83fc9a0
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-09-10 18:43:01 +0100

    Move `LogCallerFinder` under `commonMain`

--- logging/src/commonMain/kotlin/io/spine/logging/backend/LogCallerFinder.kt
+++ logging/src/commonMain/kotlin/io/spine/logging/backend/LogCallerFinder.kt
@@ -34,7 +34,7 @@ import kotlin.reflect.KClass
 
 /**
  * API for determining the logging class and log statement sites,
- * return from [Platform.getCallerFinder].
+ * return from [io.spine.logging.backend.Platform.Companion.getCallerFinder].
  *
  * These classes are immutable and thread-safe.
  *
