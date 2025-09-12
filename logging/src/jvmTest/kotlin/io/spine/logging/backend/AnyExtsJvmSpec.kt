e54888315b1d01918ba9eb23c3ef3db35e9889e4
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-09-12 18:26:24 +0100

    Move `backend` tests under `commonTest`

--- logging/src/jvmTest/kotlin/io/spine/logging/backend/AnyExtsJvmSpec.kt
+++ logging/src/jvmTest/kotlin/io/spine/logging/backend/AnyExtsJvmSpec.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023, The Flogger Authors; 2025, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,52 +26,23 @@
 
 package io.spine.logging.backend
 
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
-import io.kotest.matchers.types.shouldBeSameInstanceAs
 import io.spine.logging.backend.given.BadToString
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-/**
- * Tests for extensions of `Any?` declared in the `AnyExts.kt` file.
- *
- * @see <a href="https://github.com/google/flogger/blob/cb9e836a897d36a78309ee8badf5cad4e6a2d3d8/api/src/test/java/com/google/common/flogger/backend/MessageUtilsTest.java">
- *   Original Java code</a> for historical context.
- */
-@DisplayName("`Any?` extensions should")
-internal class AnyMessagesSpec {
+@DisplayName("Any? extensions under JVM should")
+internal class AnyExtsJvmSpec {
 
-    @Nested
-    inner class
+    @Nested inner class
     `safely convert to string` {
-
-        @Test
-        fun `literals and arrays`() {
-            "Hello World".safeToString() shouldBeSameInstanceAs "Hello World"
-            10.safeToString() shouldBe "10"
-            false.safeToString() shouldBe "false"
-
-            // Not what you would normally get from `Any.toString()` ...
-            arrayOf("Foo", "Bar").safeToString() shouldBe "[Foo, Bar]"
-            arrayOf(1, 2, 3).safeToString() shouldBe "[1, 2, 3]"
-            null.safeToString() shouldBe "null"
-        }
 
         @Test
         fun `objects that return 'null' on 'toString()'`() {
             val badToString = BadToString()
             badToString.safeToString() shouldContain badToString::class.simpleName!!
             badToString.safeToString() shouldContain "toString() returned null"
-        }
-
-        @Test
-        fun `objects that throw on 'toString()'`() {
-            val any = object {
-                override fun toString(): String = throw IllegalArgumentException("Badness")
-            }
-            any.safeToString() shouldContain "java.lang.IllegalArgumentException: Badness"
         }
     }
 }
